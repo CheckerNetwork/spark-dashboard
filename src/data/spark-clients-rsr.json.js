5839647e47c7f2db750c07cdc2f597d5cfa5f6b7
Nikolas Haimerl <nikolas@meridian.space>
2025-03-04 10:29:55 +0100

    changed past summaries

--- src/data/spark-clients-rsr.json.js
+++ src/data/spark-clients-rsr.json.js
@@ -1,7 +1,7 @@
 import { jsonFetcher } from './json-fetcher.js'
 import { getDateXDaysAgo } from '../utils/date-utils.js'
 
-const from = getDateXDaysAgo(31)
+const from = getDateXDaysAgo(14)
 const to = getDateXDaysAgo(1)
 
 const output = await jsonFetcher(
